201f521a01e2ac93ae4e9a67dc99a8d904ba699e
Hafsa Hafeez Siddiqui <hafsa.siddiqui@syslab.ai>
2025-05-02 21:55:01 +0500

    feat: add 11labs tts API

--- ravi-backend/app/services/tts_11labs.py
+++ ravi-backend/app/services/tts_11labs.py
@@ -1,9 +1,7 @@
-#sk_b202f6ad0725fd19e623700a7dd290f81a2a6606c31fd7b0 > haffaomer22@gmail.com
-
 import requests
 
-API_KEY = "sk_b202f6ad0725fd19e623700a7dd290f81a2a6606c31fd7b0"
-VOICE_ID = "pNInz6obpgDQGcFmaJgB"  
+API_KEY = ""
+VOICE_ID = ""  
 
 url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}"
 
@@ -13,7 +11,7 @@ headers = {
 }
 
 data = {
-    "text": "السلام علیکم! یہ ElevenLabs کی اردو ٹیکسٹ ٹو اسپیچ سروس کا ایک ڈیمو ہے۔",
+    "text": "آپ کیسے ہیں",
     "model_id": "eleven_multilingual_v1",
     "voice_settings": {
         "stability": 0.7,           # smoother speech
@@ -27,6 +25,6 @@ response = requests.post(url, headers=headers, json=data)
 with open("urdu_output.mp3", "wb") as f:
     f.write(response.content)
 
-print("✅ Urdu speech generated and saved as urdu_output.mp3")
+print("Urdu speech generated and saved as urdu_output.mp3")
 
 
